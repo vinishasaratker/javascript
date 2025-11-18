900345ef07eedd86ba025111b4dbb47139e51770
vinishasaratker <vinishasaratker@gmail.com>
2025-11-18 12:02:54 +0530

    advance dom use

--- dommm.js
+++ dommm.js
@@ -2,7 +2,26 @@
 let jstext=()=>{
   let h1tag=document.getElementById('text'); 
   
-  // agehum button par click krenge tab yeh text show honga 
-    h1tag.innerHTML="hyy this is backend text";
+  // agr hum button par click krenge tab yeh text show honga 
+    h1tag.innerHTML="<u>hyy this is backend text</u>";
+   let ptag=  document.querySelector('.para');
+   ptag.style.color="red";
+
+//    <!-- cammel case ka use krna hai properties ke liye 1st element small honga or 
+//  baki ke second vale capital -->
+   ptag.style.fontSize="50px";
+   ptag.style.backgroundColor="yellow"
+   ptag.style.fontWeight="600"
 }
- 
+let box1=()=>{
+   let divtag=document.querySelector('#main');
+   divtag.style.backgroundColor="red"
+}
+ let box2=()=>{
+   let divtag=document.querySelector('#main');
+   divtag.style.backgroundColor="yellow"
+}
+let box3=()=>{
+   let divtag=document.querySelector('#main');
+   divtag.style.backgroundColor="green"
+}
