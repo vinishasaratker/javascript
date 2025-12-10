78cb6bb237cbd810aef63039f3273cfe266ebcb5
vinishasaratker <vinishasaratker@gmail.com>
2025-12-10 11:52:46 +0530

    hyy

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-// let firstpromise=new Promise((reset,resolve)=>
+// let firstpromise=new Promise((reject,resolve)=>
 // {
 //     console.log(" hyyyu");
 //  reject(new Error("internal error") )
@@ -7,12 +7,53 @@
 // )
 // asynchronous code
 
-// let promise2=new Promise((reset,resolve)=>{
+// let promise2=new Promise((reject,resolve)=>{
 //  setTimeout(function saymyname(){
 //     console.log("my name is vinisha")
 //  },2000)
 // }
 // )
-let promise3=new promise((resolve,reject)=>{
+// let promise3=new Promise((resolve,reject)=>{
+// let success=true;
+// // let success=false  // bhi kr skte hai to catch block execute ho jayenga 
+// if(success){
+//     resolve(" promise fullfilled")
 
+// }
+// else{
+//     reject("promise rejected")
+// }
+// })
+// promise3.then((message)=>{
+// console.log("then ka message h yeh :  "+   message)
+
+// })
+// .catch((error)=>{
+// console.log("error  :"+  error)
+// })
+
+// **** promise chainn
+
+let promise5=new Promise((resolvem,reject)=>
+{
+    let success=false;
+    if(success){
+        resolvem(10)   }
+
+    else{  reject(-6)}
+})
+promise5.then((message)=>{
+console.log(" first messsage: "+message)
+return 30;
+})
+.then((message)=>{
+    console.log("second message :" + message)
+    return 70;
+
+})
+.then((message)=>{
+    console.log("third message :"+message)
+})
+.catch((error)=>{
+    console.error(error)
 })
