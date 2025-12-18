07af526a602179e86e1452a56c4833d0e076d338
vinishasaratker <vinishasaratker@gmail.com>
2025-12-18 17:37:42 +0530

    hyy

--- fetch.js
+++ fetch.js
@@ -5,6 +5,7 @@ const getfacts=async()=>{
 
     console.log(" getting data ...")
     let response= await fetch(Url);
+    
     console.log(response)// it is in json format 
     let data= await response.json();
      factpara.innerText=(data[0]).text;
