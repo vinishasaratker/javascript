0721d479aa9fa767e1609c0b71954d7b5eecfd59
vinishasaratker <vinishasaratker@gmail.com>
2025-12-27 12:35:40 +0530

    my

--- usebom.js
+++ usebom.js
@@ -11,41 +11,41 @@
 //    let ans1=  prompt(" enter number 1")
 //    let ans2= prompt(" enter number2")
 //   alert (parseInt(ans1)+ parseInt(ans2))
-     // there three method to convert integer to string
-  //1 number()
-  // 2. parseInt() 
-  // 3. parsefloat()
+//      there three method to convert integer to string
+//   1 number()
+//   2. parseInt() 
+//   3. parsefloat()
 
 
-//             let age =prompt(" enter your age")
-// if (age>=18){
-//     alert(" you can vote")
-// }
-// else(
-//     alert(" you cant vote")
-// )
+            let age =prompt(" enter your age")
+if (age>=18){
+    alert(" you can vote")
+}
+else(
+    alert(" you cant vote")
+)
  
-// let number=prompt(" enter any number")
-// if(number>0){
-//     alert(" number is positive")
-// }
-// else if(number==0){
-//     alert(" number is zero")
-// }
-// else if(number<0){
-//     alert(" number is negative")
-// }
-// }
-// function  bom(){
-// let  a=5;
-// let b=6;
-// alert("sum of a+b ="+(a+b))
-//alert("sum of"+a + "+"+b+"="+(a+b))
+let number=prompt(" enter any number")
+if(number>0){
+    alert(" number is positive")
+}
+else if(number==0){
+    alert(" number is zero")
+}
+else if(number<0){
+    alert(" number is negative")
+}
 
-// prompt---
-//  let ans=prompt(" hi vini")
-//  alert(ans)
-// }
-// bom();
+function  bom(){
+let  a=5;
+let b=6;
+alert("sum of a+b ="+(a+b))
+alert("sum of"+a + "+"+b+"="+(a+b))
+
+//prompt---
+ let ans=prompt(" hi vini")
+ alert(ans)
+}
+bom();
 
 
