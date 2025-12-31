8b6e1e2ff3656eff50be5dffeaad1bff0393d122
vinishasaratker <vinishasaratker@gmail.com>
2025-12-31 15:07:24 +0530

    weather app

--- filters.js
+++ filters.js
@@ -1,4 +1,10 @@
-let method1=()=>{
+let method1 = () => {
 
+    const mynum = [2,6,89,0,87,54,54,34,12,9865];
+    const newnum = mynum.filter((num) => {
+         return num > 8
+
+    });
+    console.log(newnum);
 }
-method1()
+method1();
