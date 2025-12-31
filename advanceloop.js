f5a7c8af201c0ceab81e45ee997019e5b2afbc32
vinishasaratker <vinishasaratker@gmail.com>
2025-12-31 09:39:47 +0530

    advace loops

---
+++ advanceloop.js
@@ -0,0 +1,21 @@
+let vini=()=>{
+    const arrr=[5,7,9,5,0]
+    for (const num of arrr) {
+        console.log(num);
+         }
+         let fruit=["mango&apple"]
+         for (const greet of fruit) {
+            console.log(`each charactoris ${greet}`)
+            
+         }
+}
+let motu=()=>{
+    const name=["vujgfj","hiyy","ljga","opriya","gifi ji "]
+    for (const namesare of name) {
+     console.log(namesare) 
+     console.log(`charactor is ${namesare}`)  
+    }
+}
+
+vini();
+motu();
