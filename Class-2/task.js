27a9a6fdedef24d95a422aa8cff04bc3b9e536d9
vinishasaratker <vinishasaratker@gmail.com>
2026-01-14 14:56:45 +0530

    mycode

---
+++ Class-2/task.js
@@ -0,0 +1,19 @@
+let calculation = () => {
+
+    let age = Number(document.getElementById("age").value);
+    let height = Number(document.getElementById("height").value);
+    let weight = Number(document.getElementById("weight").value);
+
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+
+    let bmr;
+    if (gender === "male") {
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    } else {
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+    }
+
+    document.getElementById("demo").innerHTML = "Your BMR is : " + bmr;
+};
+
+document.getElementById("btn1").addEventListener("click", calculation);
